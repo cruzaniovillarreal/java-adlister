35940ff987879b493b092d27c499a4596bb356bb
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2020-11-03 11:17:57 -0600

    added try catch statements for exceptions

--- src/main/java/CounterServlet.java
+++ src/main/java/CounterServlet.java
@@ -8,15 +8,18 @@ import java.io.IOException;
 @WebServlet(name = "CounterServlet", urlPatterns = "/counter")
 public class CounterServlet extends HttpServlet {
     private int counter = 0;
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        counter += 1;
-        response.getWriter().println("<h1>The count is "+counter+".</h1>");
-        String toReset = request.getParameter("reset");
-        if (toReset.equalsIgnoreCase("reset")) {
-            this.counter = 0;
+        try {
+            counter += 1;
+            response.getWriter().println("<h1>The count is " + counter + ".</h1>");
+            String toReset = request.getParameter("reset");
+            if (toReset.equalsIgnoreCase("reset")) {
+                this.counter = 0;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-
     }
+
 }
--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -8,14 +8,18 @@ import java.io.PrintWriter;
 @WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
 
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
-        res.setContentType("text/html");
-        PrintWriter out =res.getWriter();
-        String name = req.getParameter("name");
-        if (name == null || name.equals("")) {
-            name = "World";
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+        try {
+            res.setContentType("text/html");
+            PrintWriter out =res.getWriter();
+            String name = req.getParameter("name");
+            if (name == null || name.equals("")) {
+                name = "World";
+            }
+            out.println("<h3>Hello "+name+".</h3>");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        out.println("<h3>Hello "+name+".</h3>");
     }
 
 }
