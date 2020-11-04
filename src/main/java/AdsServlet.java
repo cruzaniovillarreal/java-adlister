dd16f4ede68054c4bba10552120e51b01121c9d7
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2020-11-04 15:35:43 -0600

    created example and login/ads servlet

--- src/main/java/AdsServlet.java
+++ src/main/java/AdsServlet.java
@@ -5,10 +5,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "AdServlet", urlPatterns = "/ads")
-public class AdServlet extends HttpServlet {
+@WebServlet(name = "AdsServlet", urlPatterns = "/ads")
+public class AdsServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("../webapp/ads/index.jsp").forward(request, response);
+        request.getRequestDispatcher("/ads/index.jsp").forward(request, response);
     }
+
 }
