b1765a0eceff40ab2707c935ec9289c24e9dd536
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2020-11-05 13:41:32 -0600

    finished mvc-exercises

--- src/main/java/AdsServlet.java
+++ src/main/java/AdsServlet.java
@@ -4,12 +4,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "AdsServlet", urlPatterns = "/ads")
 public class AdsServlet extends HttpServlet {
+    private Ads adsManager;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("ads", adsManager.all());
         request.getRequestDispatcher("/ads/index.jsp").forward(request, response);
     }
 
+    @Override
+    public void init() {
+        adsManager = DaoFactory.getAdsDao();
+    }
 }
