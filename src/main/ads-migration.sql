cd4255785023f76a84f9fe3fbea280395488acc0
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2020-11-09 14:14:33 -0600

    completed jdbc exercise

--- src/main/ads-migration.sql
+++ src/main/ads-migration.sql
@@ -1,20 +1,26 @@
+USE adlister_db;
+
 DROP TABLE IF EXISTS ads;
 DROP TABLE IF EXISTS users;
 
-CREATE TABLE users (
-id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-username VARCHAR(50) NOT NULL,
-email VARCHAR(65) NOT NULL,
-password VARCHAR(45) NOT NULL,
-PRIMARY KEY(id)
+CREATE TABLE users
+(
+    id       INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    username VARCHAR(240) NOT NULL,
+    email    VARCHAR(240) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE ads (
-id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-title VARCHAR(225) NOT NULL,
-description VARCHAR(225) NOT NULL,
-user_id INT UNSIGNED NOT NULL,
-FOREIGN KEY(user_id) REFERENCES users(id)
+CREATE TABLE ads
+(
+    id          INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_id     INT UNSIGNED NOT NULL,
+    title       VARCHAR(240) NOT NULL,
+    description TEXT         NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+        ON DELETE CASCADE
 );
 
 
--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -6,11 +6,11 @@ import com.mysql.cj.jdbc.Driver;
 import java.sql.*;
 
 public class MySQLAdsDao implements Ads {
-    private List<Ad> ads;
     private Connection connection = null;
     private Config config = new Config();
+    private List<Ad> ads;
 
-    public MySQLAdsDao() {
+    public MySQLAdsDao(){
         try {
             DriverManager.registerDriver(new Driver());
             connection = DriverManager.getConnection(
@@ -25,10 +25,11 @@ public class MySQLAdsDao implements Ads {
 
     public List<Ad> all() {
         Statement stmt = null;
+        List<Ad> ads = new ArrayList<>();
         try {
             stmt = connection.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM ads");
-            while(rs.next()) {
+            ResultSet rs = stmt.executeQuery("select * from ads");
+            while(rs.next()){
                 ads.add(new Ad(
                         rs.getLong("id"),
                         rs.getLong("user_id"),
@@ -37,14 +38,13 @@ public class MySQLAdsDao implements Ads {
                 ));
             }
         } catch (SQLException throwables) {
-           throwables.printStackTrace();
+            throwables.printStackTrace();
         }
         return ads;
     }
 
     public Long insert(Ad ad) {
-        String sql = String.format("insert into ads (user_id, title, description) values(%d, '%s', '%s')", ad.getUserId(), ad.getTitle(), ad.getDescription());
-
+        String sql = String.format("insert into ads (user_id, title, description) values(%d, '%s', '%s')", ad.getUserId() ,ad.getTitle(), ad.getDescription());
         try {
             Statement stmt = connection.createStatement();
             stmt.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
@@ -54,7 +54,6 @@ public class MySQLAdsDao implements Ads {
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
-
         return 0L;
     }
 }
