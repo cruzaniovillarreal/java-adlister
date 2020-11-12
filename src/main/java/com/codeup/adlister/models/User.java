cb33c5300045afcd6902b72c150ca033b912ecce
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2020-11-12 14:09:57 -0600

    hashed passwords, added check logic for login, reworked ads to be assigned id of logged in user

--- src/main/java/com/codeup/adlister/models/User.java
+++ src/main/java/com/codeup/adlister/models/User.java
@@ -1,24 +1,29 @@
 package com.codeup.adlister.models;
 
+import org.mindrot.jbcrypt.BCrypt;
+
 public class User {
     private long id;
     private String username;
     private String email;
     private String password;
 
+    //ANYTHING
     public User() {}
 
+    //SELECT
     public User(String username, String email, String password) {
         this.username = username;
         this.email = email;
         this.password = password;
     }
 
+    //INSERT (sql)
     public User(long id, String username, String email, String password) {
         this.id = id;
         this.username = username;
         this.email = email;
-        this.password = password;
+        this.setPassword(password);
     }
 
     public long getId() {
@@ -50,6 +55,6 @@ public class User {
     }
 
     public void setPassword(String password) {
-        this.password = password;
+        this.password = BCrypt.hashpw(password, BCrypt.gensalt());
     }
 }
